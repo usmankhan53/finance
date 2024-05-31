04d58282684a0cf602fbfdde96e2d033721550c6
usmankhan53 <128886169+usmankhan53@users.noreply.github.com>
2024-06-01 04:22:58 +0500

    Update url BuyPage.jsx

--- client/src/pages/BuyPage.jsx
+++ client/src/pages/BuyPage.jsx
@@ -56,7 +56,7 @@ const BuyPage = () => {
       return;
     }
 
-    const apiEndpoint = `https://erpfinance.netlify.app/.netlify/functions/app/purchase/${category}`;
+    const apiEndpoint = `https://financelocal.netlify.app/functions/app/purchase/${category}`;
     const method = 'PUT';
 
     try {
@@ -97,7 +97,7 @@ const BuyPage = () => {
     const userResponse = prompt("Are you sure you want to delete it?", "Yes");
     if (userResponse === "Yes") {
       try {
-        const response = await fetch(`https://erpfinance.netlify.app/.netlify/functions/app/purchase/${category}/${purchaseId}`, {
+        const response = await fetch(`https://financelocal.netlify.app/functions/app/purchase/${category}/${purchaseId}`, {
           method: 'PUT',
         });
         if (!response.ok) throw new Error('Failed to delete purchase record');
